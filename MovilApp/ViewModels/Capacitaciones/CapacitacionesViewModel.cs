c73c762d5837072597ecf0d52876b3b935409fed
ASUS X515M <ASUS X515M@XIMENA>
2025-10-06 23:30:17 -0300

    Añadir vista de capacitaciones y su ViewModel
    Se ha implementado una nueva interfaz de usuario en `CapacitacionesView.xaml` para buscar capacitaciones, que incluye un `SearchBar`, un botón de búsqueda y un `CollectionView` para mostrar resultados.
    
    Se ha creado el `CapacitacionesViewModel` en `CapacitacionesViewModel.cs` para gestionar la lógica de búsqueda y el estado de carga, utilizando el patrón MVVM con CommunityToolkit.Mvvm.
    
    Además, se ha añadido un nuevo `FlyoutItem` en `AgoraShell.xaml` para acceder a la vista de capacitaciones desde el menú de navegación, y se han actualizado las configuraciones en `MovilApp.csproj` para incluir el nuevo archivo XAML en el proyecto.

---
+++ MovilApp/ViewModels/Capacitaciones/CapacitacionesViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Service.Models;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovilApp.ViewModels.Capacitaciones
+{
+    partial class CapacitacionesViewModel : ObservableObject
+    {
+        GenericServices<Capacitacion> _capacitacionService = new();
+
+        [ObservableProperty]
+        private string textobusqueda = string.Empty;
+
+        [ObservableProperty]
+        private bool estaDescargando;
+
+        [ObservableProperty]
+        private ObservableCollection<Capacitacion> capacitaciones = new();
+
+
+        public IRelayCommand BuscarCommand { get; }
+        public IRelayCommand LimpiarCommand { get; }
+
+        public CapacitacionesViewModel()
+        {
+            BuscarCommand = new RelayCommand(OnBuscar);
+            LimpiarCommand = new RelayCommand(OnLimpiar);
+            _ = InicializarAsync();
+        }
+
+        private async Task InicializarAsync()
+        {
+            OnBuscar();
+        }
+
+        private async void OnBuscar()
+        {
+            if (EstaDescargando) return;
+
+            try
+            {
+                EstaDescargando = true;
+
+                // Obtener todos los libros si no los tenemos
+                var capacitaciones = await _capacitacionService.GetAllAsync(textobusqueda);
+
+                Capacitaciones = capacitaciones != null ?
+                        new ObservableCollection<Capacitacion>(capacitaciones)
+                        : new ObservableCollection<Capacitacion>();
+            }
+            finally
+            {
+                EstaDescargando = false;
+            }
+        }
+        private void OnLimpiar()
+        {
+            Textobusqueda = string.Empty;
+            // Mantener los filtros pero ejecutar búsqueda limpia
+            OnBuscar();
+        }
+    }
+}
+
