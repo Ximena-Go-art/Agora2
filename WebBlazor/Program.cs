431258aa0579bc7e944f48e85eba74e8996afd26
ASUS X515M <ASUS X515M@XIMENA>
2025-10-21 19:40:46 -0300

    Añadir pruebas y mejorar la visualización de capacitaciones
    Se ha implementado un nuevo método de prueba `TestGetCapacitacionesFuturasAsync` en `UnitTestCapacitacionServices.cs` para verificar el correcto funcionamiento del servicio `CapacitacionService`.
    
    En `Home.razor`, se ha mejorado la lógica de carga y visualización de capacitaciones abiertas, utilizando el componente `CardCapacitacion` para mostrar la información de manera más atractiva.
    
    Se han añadido servicios para `IInscripcionService` y `ICapacitacionService` en `Program.cs`, y se ha actualizado `_Imports.razor` para incluir componentes de `WebBlazor`.
    
    Además, se ha creado el componente `CardCapacitacion.razor` para presentar la información de las capacitaciones, y se ha añadido un estilo básico en `CardCapacitacion.css`.

--- WebBlazor/Program.cs
+++ WebBlazor/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddSweetAlert2();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericServices<>));
+builder.Services.AddScoped<IInscripcionService, InscipcionService>();
+builder.Services.AddScoped<ICapacitacionService, CapacitacionService>();
 builder.Services.AddScoped <FirebaseAuthService>();
 
 await builder.Build().RunAsync();
